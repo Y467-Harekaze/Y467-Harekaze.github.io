c4fe5ca81b225e3172f0ac530d27e8d5ca9f8730
Y467-Harekaze <84701134+Y467-Harekaze@users.noreply.github.com>
2021-10-14 21:19:03 +0800

    bug fix

--- www/service-worker.js
+++ www/service-worker.js
@@ -19,78 +19,78 @@
 
 // CODELAB: Update cache names any time any of the cached files change.
 var today = new Date();
-var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-var dateTime = date+'_'+time;
+var dateTime = date + '_' + time;
 const CACHE_NAME = dateTime;
 const DATA_CACHE_NAME = dateTime;
 
 // CODELAB: Add list of files to cache here.
 const FILES_TO_CACHE = [
-	
+
 ];
 
 self.addEventListener('install', (evt) => {
-  console.log('[ServiceWorker] Install');
-  // CODELAB: Precache static resources here.
-  
-  // CODELAB: Precache static resources here.
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('[ServiceWorker] Pre-caching offline page');
-      return cache.addAll(FILES_TO_CACHE);
-    })
-);
+	console.log('[ServiceWorker] Install');
+	// CODELAB: Precache static resources here.
 
-  self.skipWaiting();
+	// CODELAB: Precache static resources here.
+	evt.waitUntil(
+		caches.open(CACHE_NAME).then((cache) => {
+			console.log('[ServiceWorker] Pre-caching offline page');
+			return cache.addAll(FILES_TO_CACHE);
+		})
+	);
+
+	self.skipWaiting();
 });
 
 self.addEventListener('activate', (evt) => {
-  console.log('[ServiceWorker] Activate');
-  // CODELAB: Remove previous cached data from disk.
-  
-  evt.waitUntil(
-    caches.keys().then(keyList => Promise.all(keyList.map((key) => {
-      if (key !== CACHE_NAME) {
-        console.log('[ServiceWorker] Removing old cache', key)
-        return caches.delete(key)
-      }
-    })))
-  )
-  
-  self.clients.claim();
+	console.log('[ServiceWorker] Activate');
+	// CODELAB: Remove previous cached data from disk.
+
+	evt.waitUntil(
+		caches.keys().then(keyList => Promise.all(keyList.map((key) => {
+			if (key !== CACHE_NAME) {
+				console.log('[ServiceWorker] Removing old cache', key)
+				return caches.delete(key)
+			}
+		})))
+	)
+
+	self.clients.claim();
 });
 
 self.addEventListener('fetch', (evt) => {
-  console.log('[ServiceWorker] Fetch', evt.request.url);
-  // CODELAB: Add fetch event handler here.
-  
-  // CODELAB: Add fetch event handler here.
-if (evt.request.url.includes('/mayday_mayday_mayday/')) {
-  console.log('[Service Worker] Fetch (data)', evt.request.url);
-  evt.respondWith(
-      caches.open(DATA_CACHE_NAME).then((cache) => {
-        return fetch(evt.request)
-            .then((response) => {
-              // If the response was good, clone it and store it in the cache.
-              if (response.status === 200) {
-                cache.put(evt.request.url, response.clone());
-              }
-              return response;
-            }).catch((err) => {
-              // Network request failed, try to get it from the cache.
-              return cache.match(evt.request);
-            });
-      }));
-  return;
-}
-evt.respondWith(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.match(evt.request)
-          .then((response) => {
-            return response || fetch(evt.request);
-          });
-    })
-);
-  
+	console.log('[ServiceWorker] Fetch', evt.request.url);
+	// CODELAB: Add fetch event handler here.
+
+	// CODELAB: Add fetch event handler here.
+	if (evt.request.url.includes('/mayday_mayday_mayday/')) {
+		console.log('[Service Worker] Fetch (data)', evt.request.url);
+		evt.respondWith(
+			caches.open(DATA_CACHE_NAME).then((cache) => {
+				return fetch(evt.request)
+					.then((response) => {
+						// If the response was good, clone it and store it in the cache.
+						if (response.status === 200) {
+							cache.put(evt.request.url, response.clone());
+						}
+						return response;
+					}).catch((err) => {
+						// Network request failed, try to get it from the cache.
+						return cache.match(evt.request);
+					});
+			}));
+		return;
+	}
+	evt.respondWith(
+		caches.open(CACHE_NAME).then((cache) => {
+			return cache.match(evt.request)
+				.then((response) => {
+					return response || fetch(evt.request);
+				});
+		})
+	);
+
 });
