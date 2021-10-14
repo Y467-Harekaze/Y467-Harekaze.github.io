a9d15632ef1ee45dcf0aefa75490e942898ec73a
Y467-Harekaze <84701134+Y467-Harekaze@users.noreply.github.com>
2021-10-14 21:06:23 +0800

    bug fix

--- www/service-worker_register.js
+++ www/service-worker_register.js
@@ -2,7 +2,7 @@
 //service-worker.js must put in root directory
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('service-worker.js', { scope: '/www/' })
+    navigator.serviceWorker.register('service-worker.js', { scope: '/' })
         .then((reg) => {
           console.log('Service worker registered.', reg);
         });
