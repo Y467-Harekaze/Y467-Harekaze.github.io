d62fbe2cb0bcf9c642d8e77e45af58cc9e6013de
Y467-Harekaze <84701134+Y467-Harekaze@users.noreply.github.com>
2021-10-14 20:51:11 +0800

    Upgrade /www/

---
+++ www/service-worker_register.js
@@ -0,0 +1,10 @@
+// CODELAB: Register service worker.
+//service-worker.js must put in root directory
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('service-worker.js', { scope: '/' })
+        .then((reg) => {
+          console.log('Service worker registered.', reg);
+        });
+  });
+}
---
+++ www/universal/setting.js
@@ -0,0 +1,9 @@
+setting = [];
+
+setting['password_hash_function'] = 'sha256';
+
+setting['service_code'] = 'harekaze';//harekaze, hkfs or hkdd
+setting['title'] = 'Harekaze';//move to /values/string.php ?
+
+setting['short_product_name'] = setting['service_code'];
+setting['product_name'] = setting['title'];
