53918917e5d9a3440ba45cf876d04f558bd72d98
Y467-Harekaze <84701134+Y467-Harekaze@users.noreply.github.com>
2025-07-15 05:58:39 +0800

    upgrade website to new disign

--- universal/setting.js
+++
@@ -1,9 +0,0 @@
-setting = [];
-
-setting['password_hash_function'] = 'sha256';
-
-setting['service_code'] = 'harekaze';//harekaze, hkfs or hkdd
-setting['title'] = 'Harekaze';//move to /values/string.php ?
-
-setting['short_product_name'] = setting['service_code'];
-setting['product_name'] = setting['title'];
